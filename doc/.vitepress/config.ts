838357161e4d09210c6c39734b1a82a67c7a2091
Allen Xu <allenxuxu@foxmail.com>
2023-03-19 22:24:58 +0800

    init doc

---
+++ doc/.vitepress/config.ts
@@ -0,0 +1,29 @@
+import {defineConfig} from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+    base: '/doc/',
+    title: "人生笔记｜用户指南",
+    description: "常用功能介绍",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: '下载', link: '/'},
+        ],
+
+        sidebar: [
+            {
+                text: 'Examples',
+                items: [
+                    {text: 'Markdown Examples', link: '/markdown-examples'},
+                    {text: 'Runtime API Examples', link: '/api-examples'}
+                ]
+            }
+        ],
+
+        socialLinks: [
+            {icon: 'twitter', link: 'https://twitter.com/Allen_Xuxu'}
+        ]
+    }
+})
